0a57614ee3148b536d7f1cb3f10a81798a7e6beb
Karin Prater <53653631+gahntpo@users.noreply.github.com>
2022-04-07 13:24:03 +0300

    Add files via upload

---
+++ TestProject/Test6Project/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Test6Project
+//
+//  Created by Karin Prater on 05.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
